4740d87b0902ce157a83f826f1ad5aab5eca1b55
Maxi Prez <maxi.prez@gmail.com>
2021-10-19 11:30:57 -0300

    se corrigió el js

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,17 +4,17 @@ let txt = document.getElementById('txt');
 let txtInicio = document.getElementById('txtInicio');
 
 function numeroMoneda (){
-imagen.src = '../assets/img/loading.gif';
+imagen.src = './assets/img/loading.gif';
 txt.innerHTML = `Lanzando la moneda...`
 txtInicio.style.display = 'none';
     setTimeout(function(){
         let numeroRandom = Math.round(Math.random() * (1 - 0) + 0);
         if(numeroRandom === 1){
             txt.innerHTML = `CARA`;
-            imagen.src = '../assets/img/cara.png';
+            imagen.src = './assets/img/cara.png';
         } else{
             txt.innerHTML = `SECA`;
-            imagen.src = '../assets/img/seca.png';
+            imagen.src = './assets/img/seca.png';
             }
      }, 2000);
 }
